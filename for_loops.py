65fe370b54e5555b56174c25814e2efde0ba9d8c
hasmeen <112644288+hasmeen@users.noreply.github.com>
2022-11-16 08:23:49 -0500

    hw

--- for_loops.py
+++ for_loops.py
@@ -1,20 +1,52 @@
+# #sofia Fall
+# import random
 
-def count(first, last):
-    """
-    This function will create a string of numbers separated by a space. The numbers will start with the
-    first number and end with the second. The second number SHOULD be included as part of the string. If
-    the first number is larger than the second, the numbers should count down, rather than up.
-    count(5, 10) returns "5 6 7 8 9 10 "
-    :param first: The starting number
-    :param second: The final number. Must be included
-    :return: A string containing the numbers
-    """
-    pass # make sure to delete this line when you write your own function
+# for x in range(10):
+#     number = random.randint(1,100)
+#     print(number)
+
+# # def count(first, last):
+# #     """
+# #     This function will create a string of numbers separated by a space. The numbers will start with the
+#     first number and end with the second. The second number SHOULD be included as part of the string. If
+#     the first number is larger than the second, the numbers should count down, rather than up.
+#     count(5, 10) returns "5 6 7 8 9 10 "
+#     :param first: The starting number
+#     :param second: The final number. Must be included
+#     :return: A string containing the numbers
+#     """
+#     pass # make sure to delete this line when you write your own function
 
 
-def main():
-    print(count(0, 6))
+# def main():
+#     print(count(0, 6))
+
+
+# if __name__ == '__main__':
+#     main()
+def count(first, last, m):
+    # Create an empty string
+    string_of_numbers = ''
+    count_up = True
+
+    # Determine if counting up or down
+
+    # If m is negative
+    if m < 0:
+    # Don't count up
+    #count_up = False
+    #Swap first and last using a temp variable
+        temp = first
+        first = last
+        last = temp
+
+    # Call range with start, stop, and step
+    for number in range(first, last, m):
+        string_of_numbers += str(number) + ' '
+
+    return string_of_numbers
 
 
 if __name__ == '__main__':
-    main()
+    print(count(1, 10, 2)) # 1 3 5 7 9
+    print(count(2, 28, -3)) # 28 25 22 19 16 13 10 7 4
--- multiplication.py
+++ multiplication.py
@@ -1,7 +1,39 @@
+# def multiplication_table(number):
+#     """
+#     Ex. multiplication_table(6) returns "6 12 18 24 30 36 42 48 54 60 66 72 "
+#     :param number: An integer
+#     :return: A string of 12 values representing the mulitiplication table of the parameter number.
+#     """
+#     pass # Make sure to delete this line when writing your function.
+
 def multiplication_table(number):
-    """
-    Ex. multiplication_table(6) returns "6 12 18 24 30 36 42 48 54 60 66 72 "
-    :param number: An integer
-    :return: A string of 12 values representing the mulitiplication table of the parameter number.
-    """
-    pass # Make sure to delete this line when writing your function.
+
+    test = " "
+    for x in range(12):
+        test += str(x * number)+ " "
+    print(test)
+
+def main():
+
+       (multiplication_table(9))
+
+main()# def multiplication_table(number):
+#     """
+#     Ex. multiplication_table(6) returns "6 12 18 24 30 36 42 48 54 60 66 72 "
+#     :param number: An integer
+#     :return: A string of 12 values representing the mulitiplication table of the parameter number.
+#     """
+#     pass # Make sure to delete this line when writing your function.
+
+def multipiclation_table(number):
+
+    test = " "
+    for x in range(1, 12):
+        test += str(x * number)+ " "
+    print(test)
+
+def main():
+
+       (multipiclation_table(9))
+
+main()
